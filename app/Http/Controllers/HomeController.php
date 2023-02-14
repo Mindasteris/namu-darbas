fa9fb5d2c92f99225f7df2856a7ea23cdc76c6f5
Mindaugas Šunokas <m.sunokas@novian.lt>
2023-02-14 09:41:57 +0200

    Upload Task

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,48 +9,93 @@ class HomeController extends Controller
     /**
      * @return \Illuminate\View\View
      */
-    public function index(DriverExpenseService $driverExpenseService)
-    {
-        // Data is hardcoded for test purposes.
-        $drivers = ['John Sigono', 'Tom Davidson'];
+    // public function index(DriverExpenseService $driverExpenseService)
+    // {
+    //     // Data is hardcoded for test purposes.
+    //     $drivers = ['John Sigono', 'Tom Davidson'];
 
-        $expenses = $this->generateTestExpenses();
+    //     $expenses = $this->generateTestExpenses();
 
-        return view('home', [
-            'data' => $driverExpenseService->calculateDriverExpenses($drivers, $expenses),
-        ]);
-    }
+    //     return view('home', [
+    //         'data' => $driverExpenseService->calculateDriverExpenses($drivers, $expenses),
+    //     ]);
+    // }
 
-    private function generateTestExpenses()
-    {
-        $result = [];
+    // private function generateTestExpenses()
+    // {
+    //     $result = [];
 
-        $expenseTypes = [
-            'Fuel (EFS)', 
-            'Fuel (Comdata)', 
-            'Insurance (Truck)', 
-            'Insurance (Trailer)', 
-            'Engine oil', 
-            'Tires', 
-            'Truck wash', 
-            'Trailer wash', 
-            'Flight tickets',
-        ];
+    //     $expenseTypes = [
+    //         'Fuel (EFS)', 
+    //         'Fuel (Comdata)', 
+    //         'Insurance (Truck)', 
+    //         'Insurance (Trailer)', 
+    //         'Engine oil', 
+    //         'Tires', 
+    //         'Truck wash', 
+    //         'Trailer wash', 
+    //         'Flight tickets',
+    //     ];
 
-        $iterations = rand(1, 9);
+    //     $iterations = rand(1, 9);
 
-        for ($i = 0; $i < $iterations; $i++) 
-        {
-            $expense = $expenseTypes[$i];
+    //     for ($i = 0; $i < $iterations; $i++) 
+    //     {
+    //         $expense = $expenseTypes[$i];
             
-            $result[$expense] = number_format($this->randomDecimal(1, 5000), 2, '.', '');
+    //         $result[$expense] = number_format($this->randomDecimal(1, 5000), 2, '.', '');
+    //     }
+
+    //     return $result;
+    // }
+
+    // private function randomDecimal(float $min, float $max, int $digit = 2)
+    // {
+    //     return mt_rand($min * 10, $max * 10) / pow(10, $digit);
+    // }
+
+
+
+
+
+    /* ********************************************************************  */
+    public function index()
+    {
+        // Table Headings
+        $headings = ['Expense/Driver', 'Amount $', 'John Sigono', 'Tom Davidson'];
+
+        // Pavadinimas (išlaidų)
+        $expenses = [
+            'Fuel (EFS)',
+            'Fuel (Comdata)',
+            'Insurance (Truck)',
+            'Insurance (Trailer)',
+            'Engine Oil',
+            'Tires',
+            'Truck Wash',
+            'Trailer Wash',
+            'Flight Ticket',
+        ]; 
+
+        // Išlaidos:
+        $amount = [100, 100.01, 27.27, 11.1, 117.17, 1107.57, 7.75, 7.5, 20];
+        $amountTotal = array_sum($amount);
+
+        // Vairuotojai pasiskirsto išlaidomis
+        $driver1 = [];
+        $driver2 = [];
+    
+        foreach ($amount as $index => $value)
+        {
+            $splitExpenses = $value / 2;
+            $driver1[$index] = $splitExpenses; 
+            $driver2[$index] = $splitExpenses - 0.001;
         }
 
-        return $result;
-    }
+        // Vairuotojų viso išlaidos
+        $driver1Total = array_sum($driver1);
+        $driver2Total = array_sum($driver2);
 
-    private function randomDecimal(float $min, float $max, int $digit = 2)
-    {
-        return mt_rand($min * 10, $max * 10) / pow(10, $digit);
+        return view('home', compact(['headings','expenses', 'amount', 'amountTotal', 'driver1', 'driver2', 'driver1Total', 'driver2Total']));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,45 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- CSS -->
+        @vite(['resources/js/app.js'])
+
         <title>Test Task</title>
     </head>
     <body>
-        <div>
-            <pre>{{ print_r($data, true) }}</pre>
+        <div class="container mx-auto p-8">
+            {{-- <pre>{{ print_r($data, true) }}</pre> --}}
+
+            <h1 class="my-8 text-center text-3xl">Drivers Table (Expenses)</h1>
+
+            {{-- Table --}}
+            <div class="relative overscroll-x-auto">
+                <table>
+                    <thead>
+                        <tr>
+                            @foreach ($headings as $heading)
+                                <th>{{ $heading }}</th>
+                            @endforeach
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($expenses as $index => $expense)
+                            <tr>
+                                <td class="font-medium">{{ $expense }}</td>
+                                <td>{{ number_format($amount[$index], 2) }}</td>
+                                <td>{{ number_format($driver1[$index], 2) }}</td>
+                                <td>{{ number_format($driver2[$index], 2) }}</td>
+                            </tr>
+                        @endforeach
+                        <tr>
+                            <td class="text-right font-medium">Total:</td>
+                            <td>{{ $amountTotal }}</td>
+                            <td>{{ number_format($driver1Total, 2) }}</td>
+                            <td>{{ number_format($driver2Total, 2) }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </body>
 </html>
